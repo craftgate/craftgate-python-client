785656e40a50f59f627443b2b1adf2f1073cd62f
furkanozmen <furkan.ozmen@craftgate.io>
2025-12-29 15:21:52 +0300

    Add provider error handling to error responses

--- craftgate/response/common/error_response.py
+++ craftgate/response/common/error_response.py
@@ -1,8 +1,11 @@
+from craftgate.response.common.provider_error import ProviderError
+
 class ErrorResponse(object):
-    def __init__(self, error_code=None, error_description=None, error_group=None):
+    def __init__(self, error_code=None, error_description=None, error_group=None, provider_error=None):
         self.error_code = error_code
         self.error_description = error_description
         self.error_group = error_group
+        self.provider_error = provider_error
 
     @classmethod
     def from_dict(cls, data):
@@ -12,16 +15,23 @@ class ErrorResponse(object):
         return cls(
             error_code=data.get("errorCode") or "UNKNOWN_ERROR",
             error_description=data.get("errorDescription") or "No error description provided.",
-            error_group=data.get("errorGroup") or "Unknown"
+            error_group=data.get("errorGroup") or "Unknown",
+            provider_error=ProviderError.from_dict(data.get("providerError"))
         )
 
     def to_dict(self):
-        return {
+        result = {
             "errorCode": self.error_code,
             "errorDescription": self.error_description,
             "errorGroup": self.error_group
         }
+        if self.provider_error:
+            result["providerError"] = self.provider_error.to_dict()
+        return result
 
     def __repr__(self):
+        if self.provider_error:
+            return "ErrorResponse(error_code=%r, error_description=%r, error_group=%r, provider_error=%r)" % (
+                self.error_code, self.error_description, self.error_group, self.provider_error)
         return "ErrorResponse(error_code=%r, error_description=%r, error_group=%r)" % (
             self.error_code, self.error_description, self.error_group)
