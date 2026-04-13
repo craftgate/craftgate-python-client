c91bfd454b0e656cdf757cec2e64c2d5d884a8ce
Onur Polattimur <onur.polattimur@gmail.com>
2026-04-13 14:44:38 +0300

    Add ignoreAdvancedPosRoutingRules parameter into RoutingOptions

--- craftgate/request/dto/routing_options.py
+++ craftgate/request/dto/routing_options.py
@@ -12,7 +12,9 @@ class RoutingOptions(object):
     def __init__(
             self,
             ordering_rule: Optional[OrderingRule] = None,
-            pos_aliases: Optional[List[str]] = None
+            pos_aliases: Optional[List[str]] = None,
+            ignore_advanced_pos_routing_rules: Optional[bool] = None
     ) -> None:
         self.ordering_rule = ordering_rule
         self.pos_aliases = pos_aliases
+        self.ignore_advanced_pos_routing_rules = ignore_advanced_pos_routing_rules
