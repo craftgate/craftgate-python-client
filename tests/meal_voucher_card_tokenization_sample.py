657c07cbb4a4e4cfac95b2cb64eb5eaaf42d43b7
deryacakmak <97deryacakmak@gmail.com>
2026-06-03 11:48:11 +0300

    Add meal voucher card tokenization functionality and remove unused BKM Express methods

---
+++ tests/meal_voucher_card_tokenization_sample.py
@@ -0,0 +1,67 @@
+import os
+import unittest
+
+from craftgate import Craftgate, RequestOptions
+from craftgate.model import ApmType
+from craftgate.request import InitMealVoucherCardTokenizationRequest, MealVoucherCardTokenizationRegenerateRequest, \
+    MealVoucherCardTokenizationCompleteRequest
+from craftgate.request.dto.meal_voucher_card_tokenization_data import MealVoucherCardTokenizationData
+
+
+class MealVoucherCardTokenizationSample(unittest.TestCase):
+    API_KEY = os.environ.get("CG_API_KEY", "api-key-2")
+    SECRET_KEY = os.environ.get("CG_SECRET_KEY", "secret-key")
+    BASE_URL = os.environ.get("CG_BASE_URL", "http://localhost:8000")
+
+    @classmethod
+    def setUpClass(cls):
+        options = RequestOptions(
+            api_key=cls.API_KEY,
+            secret_key=cls.SECRET_KEY,
+            base_url=cls.BASE_URL
+        )
+        cls.meal_voucher_card_tokenization = Craftgate(options).meal_voucher_card_tokenization()
+
+    def test_init_meal_voucher_card_tokenization(self):
+        request = InitMealVoucherCardTokenizationRequest(
+            apm_type=ApmType.SETCARD,
+            meal_voucher_card_tokenization_data=MealVoucherCardTokenizationData(
+                callback_url="https://webhook.site/e806070a-da76-4d02-a67b-54ba9e8332d3"
+            )
+        )
+
+        response = self.meal_voucher_card_tokenization.init(request)
+
+        print(response.__dict__)
+        self.assertIsNotNone(response.session_id)
+        self.assertIsNotNone(response)
+
+    def test_regenerate_meal_voucher_card_tokenization(self):
+        session_id = "session-id"
+        request = MealVoucherCardTokenizationRegenerateRequest(
+            meal_voucher_card_tokenization_data=MealVoucherCardTokenizationData(
+                callback_url="https://webhook.site/e806070a-da76-4d02-a67b-54ba9e8332d3"
+            )
+        )
+
+        response = self.meal_voucher_card_tokenization.regenerate(session_id, request)
+
+        print(response.__dict__)
+        self.assertIsNotNone(response.session_id)
+        self.assertIsNotNone(response)
+
+    def test_complete_meal_voucher_card_tokenization(self):
+        session_id = "session-id"
+        request = MealVoucherCardTokenizationCompleteRequest(
+            validation_code="123456"
+        )
+
+        response = self.meal_voucher_card_tokenization.complete(session_id, request)
+
+        print(response.__dict__)
+        self.assertIsNotNone(response.session_id)
+        self.assertIsNotNone(response)
+
+
+if __name__ == "__main__":
+    unittest.main()
