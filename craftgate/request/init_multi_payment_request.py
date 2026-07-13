24d9fa15c2f030be3d445bd2b3b7972ea5408cab
Serhat Tunca <35580464+tuncaserhat@users.noreply.github.com>
2026-07-13 09:56:09 +0300

    Remove PaymentPhase field and add EnabledInstallments field for init multipayment (#34)
    * Remove PaymentPhase field and add EnabledInstallments field for init multipayment
    
    * Remove PaymentSource field from InitMultiPaymentRequest

--- craftgate/request/init_multi_payment_request.py
+++ craftgate/request/init_multi_payment_request.py
@@ -5,7 +5,6 @@ from craftgate.model.payment_source import PaymentSource
 from craftgate.model.currency import Currency
 from craftgate.model.payment_group import PaymentGroup
 from craftgate.model.payment_method import PaymentMethod
-from craftgate.model.payment_phase import PaymentPhase
 from craftgate.request.dto.payment_item import PaymentItem
 
 
@@ -16,13 +15,12 @@ class InitMultiPaymentRequest(object):
             paid_price: Optional[Decimal] = None,
             currency: Optional[Currency] = None,
             payment_group: Optional[PaymentGroup] = None,
-            payment_source: Optional[PaymentSource] = None,
             conversation_id: Optional[str] = None,
             external_id: Optional[str] = None,
             callback_url: Optional[str] = None,
-            payment_phase: PaymentPhase = PaymentPhase.AUTH,
             payment_channel: Optional[str] = None,
             enabled_payment_methods: Optional[List[PaymentMethod]] = None,
+            enabled_installments: Optional[List[int]] = None,
             card_user_key: Optional[str] = None,
             buyer_member_id: Optional[int] = None,
             allow_only_credit_card: bool = False,
@@ -44,13 +42,12 @@ class InitMultiPaymentRequest(object):
         self.paid_price = paid_price
         self.currency = currency
         self.payment_group = payment_group
-        self.payment_source = payment_source
         self.conversation_id = conversation_id
         self.external_id = external_id
         self.callback_url = callback_url
-        self.payment_phase = payment_phase
         self.payment_channel = payment_channel
         self.enabled_payment_methods = enabled_payment_methods
+        self.enabled_installments = enabled_installments
         self.masterpass_gsm_number = masterpass_gsm_number
         self.masterpass_user_id = masterpass_user_id
         self.apm_user_identity = apm_user_identity
